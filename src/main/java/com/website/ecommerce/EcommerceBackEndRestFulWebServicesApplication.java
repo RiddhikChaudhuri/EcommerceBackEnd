3e98fed53c7ff4c4499817471476d0c6a549e454
Sambuddha Chaudhuri <Sambuddha.Chaudhuri@tomtom.com>
2019-04-06 22:04:10 +0530

    Added Starting Initializer Files

---
+++ src/main/java/com/website/ecommerce/EcommerceBackEndRestFulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.website.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackEndRestFulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackEndRestFulWebServicesApplication.class, args);
+	}
+
+}
